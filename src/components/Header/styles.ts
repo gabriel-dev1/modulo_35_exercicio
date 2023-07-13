a304f8f023ca98d86def091f25e4f5a9ffb2c7b0
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-13 16:48:57 -0300

    Upload

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,22 +1,23 @@
 import styled from 'styled-components'
+import { cores } from '../../globalStyles'
 
 export const HeaderStyle = styled.header`
- /*  background-color: #111; */
-  background-color: #f44444;
+  background-color: ${cores.vermelho};
   padding: 20px;
   border-radius: 16px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 40px;
+  border-bottom: 1px solid ${cores.cinza};
 
   a {
-    color: lavender;
+    color: ${cores.branco};
     font-weight: bold;
     text-decoration: none;
 
     &:hover {
-        color: #a2a2a2; /*  #2f4f4f #708090 #191970 */
+      color: ${cores.LinkColor};
     }
   }
 
@@ -24,25 +25,18 @@ export const HeaderStyle = styled.header`
     display: flex;
     align-items: center;
   }
-
-  p {
-    color: #eee;
-  }
 `
 
 export const Title = styled.h1`
-  /* color: #ddd;
-  background-color: #333; */
-  color: #555;
-  background-color: lavender;
-  /* background-color: #e2e2e2; */
+  color: ${cores.cinza};
+  background-color: ${cores.branco};
   padding: 8px;
   border-radius: 8px;
 `
 
 export const List = styled.ul`
-   display: flex;
-   margin-left: 40px;
+  display: flex;
+  margin-left: 40px;
 `
 
 export const ListItem = styled.li`
