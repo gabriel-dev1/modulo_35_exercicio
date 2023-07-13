4d42d9782c277992450d2aeb57d10519a4b5958c
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-13 16:57:31 -0300

    Upload

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 import { Props } from '.'
 import { Card } from '../Product/styles'
+import { cores } from '../../globalStyles'
 
 export const Section = styled.section<Omit<Props, 'title' | 'products'>>`
   padding: 32px 0;
-  background-color: ${(props) => props.background === 'white' ? 'lavender' : '#f44444'};
+  background-color: ${(props) => props.background === 'white' ?  cores.branco : cores.vermelho};
 
   ${Card} {
-    background-color: ${(props) => props.background === 'red' ? 'lavender' : '#f44444'};
+    background-color: ${(props) => props.background === 'red' ?  cores.branco : cores.vermelho};
   }
 
   ul {
@@ -19,5 +20,5 @@ export const Section = styled.section<Omit<Props, 'title' | 'products'>>`
 
 export const Title = styled.h2`
   margin-bottom: 16px;
-  color: #555;
+  color: ${cores.cinza};
 `
