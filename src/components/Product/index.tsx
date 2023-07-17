ae553ff790f677187db76f7f22283cd9fa49767d
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-17 20:06:43 -0300

    Atualizando o upload

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,27 +1,29 @@
+import { Link } from 'react-router-dom'
 import Tag from '../Tags'
-import { Card, Image, Infos } from './styles'
+import { Card, CardFooter, Image, Infos } from './styles'
 
 type Props = {
   title: string
   description: string
   image: string
-  infos: string[]
+  infos?: string[]
 }
 
 const Product = ({ title, description, image, infos }: Props) => {
   return (
-    <>
       <Card>
         <Image src={image} />
         <Infos>
-          {infos.map((info) => (
+          {infos?.map((info) => (
             <Tag key={info}>{info}</Tag>
           ))}
         </Infos>
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <CardFooter>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <Link to="/new">Saiba Mais</Link>
+        </CardFooter>
       </Card>
-    </>
   )
 }
 
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,21 +1,43 @@
 import styled from 'styled-components'
 import { TagStyle } from '../Tags/styles'
-import { cores } from '../../globalStyles'
+/* import { cores } from '../../globalStyles' */
 
 export const Card = styled.div`
-  padding: 8px;
-  border-radius: 8px;
-  color: ${cores.cinza};
+  color: #e66767;
+  background-color: #ffffff;
   position: relative;
+  width: 444px;
+  height: 50%;
+
+  p {
+    padding: 16px 0;
+  }
 
   ${TagStyle} {
     margin-right: 8px;
   }
+
+  a {
+    background-color: #e66767;
+    color: #ffffff;
+    text-decoration: none;
+    padding: 4px;
+    font-weight: bold;
+    font-size: 14px;
+  }
+`
+
+export const CardFooter = styled.div`
+  background-color: #fff;
+  padding: 8px;
+  padding-top: 0;
+  border: 1px solid #e66767;
+  border-top: none;
 `
 
 export const Image = styled.img`
   width: 100%;
-  height: 250px;
+  height: 217px;
 `
 
 export const Infos = styled.div`
