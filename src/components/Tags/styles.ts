1aaf8c4e91628b94f9d873172e25a1b93acc4dd9
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-17 20:08:07 -0300

    Atualizando o upload

--- src/components/Tags/styles.ts
+++ src/components/Tags/styles.ts
@@ -3,11 +3,10 @@ import { Props } from './index'
 import { cores } from '../../globalStyles'
 
 export const TagStyle = styled.div<Props>`
-  background-color: ${cores.cinza};
+  background-color: #e66767;
   color: ${cores.branco};
-  border-radius: 9px;
   display: inline-block;
   font-weight: bold;
-  padding: ${(props) => props.size === 'big' ? '8px 16px' : '4px 6px'};
-  font-size: ${(props) => props.size === 'big' ? '16px' : '10px'};
+  padding: 8px 16px;
+  font-size: 12px;
 `
