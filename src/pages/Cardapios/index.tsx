f03ad71822216db1c882c6158aaf1b22b14203ca
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-18 16:44:38 -0300

    Atualizando o upload

--- src/pages/Cardapios/index.tsx
+++ src/pages/Cardapios/index.tsx
@@ -1,49 +1,50 @@
-import ProductsList from '../../components/ProductsList'
 import Products from '../../models'
 import pizza from '../../assets/images/pizza.png'
 import burguer from '../../assets/images/burguer.png'
 import Header from '../../components/Header'
+import Banner from '../../components/Banner'
+import ProductsRestList from '../../components/ProductsRestList'
 
 const pizzarias : Products[] = [
     {
       id: 1,
-      title: 'Pizzarias',
-      description: 'As melhores pizzas',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: pizza,
       infos: []
     },
     {
       id: 2,
-      title: 'Hamburgueria',
-      description: 'Os melhores hamburgues',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: pizza,
       infos: []
     },
     {
       id: 3,
-      title: 'Restaurantes italianos',
-      description: 'As melhores massas',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: pizza,
       infos: []
     },
     {
       id: 4,
-      title: 'Piz',
-      description: 'As melhores pizzas',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: burguer,
       infos: []
     },
     {
       id: 5,
-      title: 'Hamburgueria',
-      description: 'As melhores massas',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: burguer,
       infos: []
     },
     {
       id: 6,
-      title: 'Hamburgueria',
-      description: 'As melhores massas',
+      title: 'Pizza Marguerita',
+      description: 'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       image: burguer,
       infos: []
     }
@@ -53,7 +54,8 @@ const Cardapios = () => {
   return (
    <>
       <Header headerProps={'profile'} />
-      <ProductsList title={'Pizzarias'} products={pizzarias} cardbackground={'vermelho'} />
+      <Banner />
+      <ProductsRestList title={''} products={pizzarias} />
    </>
   )
 }
