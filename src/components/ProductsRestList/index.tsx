d1d8b628336382d8458fdc462576cf3e736909e0
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 12:01:00 -0300

    Upload

--- src/components/ProductsRestList/index.tsx
+++ src/components/ProductsRestList/index.tsx
@@ -1,4 +1,42 @@
 import ProductRest from '../ProductsRest'
+import { Products } from '../../pages/Home'
+import { Section } from './styles'
+
+export type Props = {
+  products?: Products
+}
+
+// tentar trazer a api para este arquivo
+// products.map não é um array
+
+const ProductsRestList = ({ products }: Props) => {
+  if (!products) {
+    return <div>Carregando...</div>
+  }
+    return (
+        <Section>
+        <div className="container">
+            <ul>
+              {/* {products?.map((product) => (
+                  <li key={product.cardapio.id}>
+                    <ProductRest
+                      id={product.cardapio.id}
+                      title={product.cardapio.nome}
+                      description={product.cardapio.descricao}
+                      image={product.cardapio.foto}
+                    />
+                  </li>
+                ))} */}
+                {products.cardapio.preco}
+            </ul>
+        </div>
+        </Section>
+    )
+}
+
+export default ProductsRestList
+
+/* import ProductRest from '../ProductsRest'
 import Products from '../../models'
 import { Section } from './styles'
 
@@ -27,4 +65,4 @@ const ProductsRestList = ({ products }: Props) => {
     )
 }
 
-export default ProductsRestList
+export default ProductsRestList */
