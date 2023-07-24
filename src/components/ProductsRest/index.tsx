cc17d429c94f16a5675d244d66afd2e2057a37ea
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 19:37:58 -0300

    Upload

--- src/components/ProductsRest/index.tsx
+++ src/components/ProductsRest/index.tsx
@@ -1,56 +1,63 @@
-import { Card, CardFooter, Image } from './styles'
-import ButtonStyle from '../Button'
-import Tag from '../Tags'
+import { useState } from 'react'
+import {  Card, CardFooter, Image, ModalItems, ModalStyles } from './styles'
+import Button from '../Button'
 
 export type Props = {
   title: string
   description: string
   image: string
- id: number
+  id: number
+  porcao: string
 }
 
-const ProductRest = ({ title, description, image }: Props) => {
+interface PropsModal {
+  isVisible: boolean
+}
+
+const ProductRest = ({ title, description, image, porcao }: Props) => {
+  const [modal, setModal] = useState<PropsModal>({
+    isVisible: false
+  })
+
+  const getDescricao = (description: string) => {
+    if (description.length > 90) {
+      return description.slice(0, 80) + '...'
+    }
+  }
+
   return (
+    <>
       <Card>
-        <img src={image} alt={title} />
+        <Image src={image} alt={title} />
         <CardFooter>
           <h3>{title}</h3>
-          <p>{description}</p>
-        <ButtonStyle />
+          <p>{getDescricao(description)}</p>
+          <Button type="button" onClick={() => setModal({
+            isVisible: true
+          })}>Mais detalhes</Button>
         </CardFooter>
       </Card>
+      <ModalStyles className={modal.isVisible ? 'isVisible' : ''}>
+        <ModalItems className="container">
+          <img src={image} />
+          <div className="margin">
+            <h3>{title}</h3>
+          <div className="test">
+          <p>{description}</p>
+          <p className="porcao">{porcao}</p>
+          </div>
+          <Button>Adicionar ao carrinho - R$ 60,90</Button>
+          </div>
+        </ModalItems>
+        <div className="overlay" onClick={() => {
+          setModal({
+            isVisible: false
+          })
+        }}>
+        </div>
+      </ModalStyles>
+    </>
   )
 }
 
 export default ProductRest
-
-/* import { Card, CardFooter, Image, Infos } from './styles'
-import ButtonStyle from '../Button'
-import Tag from '../Tags'
-
-export type Props = {
-  title: string
-  description: string
-  image: string
-  infos?: string[]
-}
-
-const ProductRest = ({ title, description, image, infos }: Props) => {
-  return (
-      <Card>
-        <Image src={image} />
-        <Infos>
-          {infos?.map((info) => (
-            <Tag key={info}>{info}</Tag>
-          ))}
-        </Infos>
-        <CardFooter>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        <ButtonStyle />
-        </CardFooter>
-      </Card>
-  )
-}
-
-export default ProductRest */
--- src/components/ProductsRest/styles.ts
+++ src/components/ProductsRest/styles.ts
@@ -7,6 +7,7 @@ export const Card = styled.div`
   width: 300px;
   padding: 10px;
   line-height: 27px;
+  height: 100%;
 
   p {
     padding: 16px 0;
@@ -30,4 +31,66 @@ export const Infos = styled.div`
   position: absolute;
   top: 16px;
   right: 16px;
+`
+
+export const ModalStyles = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  align-items: center;
+  justify-content: center;
+  display: none;
+
+  &.isVisible {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.63);
+  }
+`
+
+export const ModalItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+  background-color: ${cores.vermelho};
+  color: ${cores.amarelo};
+  padding: 30px;
+
+  header {
+    img {
+      width: 100px;
+      display: block;
+      justify-content: space-between;
+    }
+  }
+
+  h3 {
+    padding-bottom: 16px;
+  }
+
+  .porcao {
+    padding: 16px 0;
+  }
+
+  img {
+    width: 280px;
+    height: 280px;
+  }
+
+  .margin {
+    margin: 0 24px;
+  }
 `
