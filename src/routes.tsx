bbb3c78b6fcb1738b7951a4a71f27b315f02822f
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 19:39:15 -0300

    Upload

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import Cardapios from './pages/Cardapios'
 const Rotas = () => (
   <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/profile" element={<Cardapios />}></Route>
+      <Route path="/profile/:id" element={<Cardapios />}></Route>
   </Routes>
 )
 
