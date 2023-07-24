cf40457c367671cff9c50bcf5de54bf816a3bbae
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 12:46:24 -0300

    Upload

--- src/pages/Cardapios/index.tsx
+++ src/pages/Cardapios/index.tsx
@@ -9,6 +9,8 @@ import { useEffect, useState } from 'react'
 import { Card, CardFooter } from '../../components/ProductsRest/styles'
 import ButtonStyle from '../../components/Button'
 import Section from '../../components/Section'
+import { Img } from '../../components/Banner/styles'
+import Tag from '../../components/Tags'
 
 /* const pizzarias : Products[] = [
     {
@@ -58,29 +60,28 @@ import Section from '../../components/Section'
 const Cardapios = () => {
   const { id } = useParams()
   const [teste, setTeste] = useState<Products>()
-  const [teste2, setTeste2] = useState<Products[]>()
 
   useEffect(() => {
     fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`).then((res) => res.json()).then((res) => setTeste(res))
-  }, [id])
-
-  useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`).then((res) => res.json()).then((res) => setTeste2(res))
   }, [id])
 
   if (!teste) {
     return <h3>carregando</h3>
   }
 
-  if (!teste2) {
-    return <h3>Carregando</h3>
-  }
-
   return (
    <>
       <Header headerProps={'profile'} />
-      <Banner produto={teste} />
-      <Section>
+      <Img style={{backgroundImage: `url(${teste.capa})`}}>
+          <div className="container">
+            <div>
+              <Tag>{teste.tipo}</Tag>
+            </div>
+            <h2>{teste.titulo}</h2>
+          </div>
+      </Img>
+      {/* <Banner produto={teste} /> */}
+     {/*  <Section>
         <div className="container">
             <ul>
              {teste2.map((t) => (
@@ -97,7 +98,7 @@ const Cardapios = () => {
               ))}
             </ul>
         </div>
-        </Section>
+        </Section> */}
      {/*  <ProductsRestList products={teste2} /> */}
    </>
   )
