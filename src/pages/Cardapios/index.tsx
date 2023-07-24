d14a5461f3c612a890fdcf636c87966f81142d19
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 12:02:26 -0300

    Upload

--- src/pages/Cardapios/index.tsx
+++ src/pages/Cardapios/index.tsx
@@ -1,11 +1,16 @@
-import Products from '../../models'
+import { Products } from '../../pages/Home'
 import pizza from '../../assets/images/pizza.png'
 import burguer from '../../assets/images/burguer.png'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import ProductsRestList from '../../components/ProductsRestList'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Card, CardFooter } from '../../components/ProductsRest/styles'
+import ButtonStyle from '../../components/Button'
+import Section from '../../components/Section'
 
-const pizzarias : Products[] = [
+/* const pizzarias : Products[] = [
     {
       id: 1,
       title: 'Pizza Marguerita',
@@ -48,14 +53,53 @@ const pizzarias : Products[] = [
       image: burguer,
       infos: []
     }
-]
+] */
 
 const Cardapios = () => {
+  const { id } = useParams()
+  const [teste, setTeste] = useState<Products>()
+  const [teste2, setTeste2] = useState<Products[]>()
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`).then((res) => res.json()).then((res) => setTeste(res))
+  }, [id])
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`).then((res) => res.json()).then((res) => setTeste2(res))
+  }, [id])
+
+  if (!teste) {
+    return <div>Carregando</div>
+  }
+
+  if (!teste2) {
+    return <div>Carregando</div>
+  }
+
   return (
    <>
       <Header headerProps={'profile'} />
-      <Banner />
-      <ProductsRestList title={''} products={pizzarias} />
+      <Banner produto={teste} />
+      {/* <Section>
+        <div className="container">
+            <ul>
+               {teste.map((t) => (
+                <li key={t.cardapio.id}>
+                <Card>
+                 <img src={t.cardapio.foto} />
+                 <CardFooter>
+                   <h3>{t.cardapio.nome}</h3>
+                   <p>{t.cardapio.descricao}</p>
+                   <ButtonStyle />
+                 </CardFooter>
+               </Card>
+                </li>
+              ))}
+              {teste.cardapio.descricao}
+            </ul>
+        </div>
+        </Section> */}
+      <ProductsRestList products={teste} />
    </>
   )
 }
