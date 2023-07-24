92c4c392dcc3178bb0c0ba65c0458ace4974c245
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 12:09:39 -0300

    Upload

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -8,24 +8,33 @@ export type Props = {
   description: string
   image: string
   infos?: string[]
+  id: number
+  avaliacao: number
 }
 
-const Product = ({ title, description, image, infos }: Props) => {
+const Product = ({ title, description, image, infos, id, avaliacao }: Props) => {
+  const getDescricao = (description: string) => {
+    if (description.length > 97) {
+      return description.slice(0, 92) + '...'
+    }
+    return description
+  }
+
   return (
       <Card>
         <Image src={image} />
         <Infos>
-          {infos?.map((info) => (
+         {infos?.map((info) => (
             <Tag key={info}>{info}</Tag>
           ))}
         </Infos>
         <CardFooter>
           <div>
             <h3>{title}</h3>
-            <img src={nota} />
+            <span>Avaliação {avaliacao}</span>
           </div>
-          <p>{description}</p>
-          <Link to="/profile">Saiba Mais</Link>
+          <p>{getDescricao(description)}</p>
+          <Link to={`/profile/${id}`}>Saiba Mais</Link>
         </CardFooter>
       </Card>
   )
