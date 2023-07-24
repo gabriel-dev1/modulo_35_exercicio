9b8f6730c8a55bb5c3c3a40de0eedf130a9d84c3
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 12:09:02 -0300

    Upload

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,37 @@
 import { Img } from './styles'
 import img from '../../assets/images/banner.jpg'
 import Tag from '../Tags'
+import { Products } from '../../pages/Home'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-const Banner = () => {
+type Props = {
+  produto: Products
+}
+
+const Banner = ({ produto }: Props) => {
+  /* const { id } = useParams()
+  const [produto, setProduto] = useState<Products>()
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
+    .then((res) => res.json())
+    .then((res) => setProduto(res))
+  }, [id])
+  */
+
+  if (!produto) {
+    return <h3>carregando</h3>
+  }
+
   return (
       <>
-      <Img style={{backgroundImage: `url(${img})`}}>
+      <Img style={{backgroundImage: `url(${produto.capa})`}}>
           <div className="container">
             <div>
-              <Tag>Italiana</Tag>
+              <Tag>{produto?.tipo}</Tag>
             </div>
-            <h2>La Dolce Vita Trattoria</h2>
+            <h2>{produto?.titulo}</h2>
           </div>
       </Img>
       </>
