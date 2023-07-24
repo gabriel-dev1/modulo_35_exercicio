a74a509401c06b2e6e4fac171a655be30b904588
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-24 19:40:42 -0300

    Upload

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,25 +1,12 @@
 import { Img } from './styles'
-import img from '../../assets/images/banner.jpg'
 import Tag from '../Tags'
 import { Products } from '../../pages/Home'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 
 type Props = {
   produto: Products
 }
 
 const Banner = ({ produto }: Props) => {
-  /* const { id } = useParams()
-  const [produto, setProduto] = useState<Products>()
-
-  useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
-    .then((res) => res.json())
-    .then((res) => setProduto(res))
-  }, [id])
-  */
-
   if (!produto) {
     return <h3>carregando</h3>
   }
