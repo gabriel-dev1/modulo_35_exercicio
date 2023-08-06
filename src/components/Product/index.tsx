734b8375f6ed9206ecc34727bd3e97ea5e9c9b3e
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-08-06 18:02:03 -0300

    Upload

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,38 +1,36 @@
 import { Link } from 'react-router-dom'
 import { Card, CardFooter, Image, Infos } from './styles'
+import { Products } from '../../pages/Home'
 import estrela from '../../assets/images/estrela.svg'
 import Tag from '../Tags'
 
 export type Props = {
-  title: string
-  description: string
-  image: string
-  id: number
-  avaliacao: number
-  tipo: string
+  item: Products
 }
 
-const Product = ({ title, description, image, id, avaliacao, tipo }: Props) => {
-  const getDescricao = (description: string) => {
-    if (description.length > 97) {
-      return description.slice(0, 200) + '...'
-    }
-    return description
+export const getDescricao = (description: string) => {
+  if (description.length > 97) {
+    return description.slice(0, 200) + '...'
   }
+  return description
+}
+
+const Product = ({ item }: Props) => {
 
   return (
       <Card>
-        <Image src={image} />
+        <Image src={item.capa} />
         <Infos>
-          <Tag>{tipo}</Tag>
+          {item.destacado ? <Tag>{item.destacado && ('Destaque da semana')}</Tag> :  ''}
+          <Tag>{item.tipo}</Tag>
         </Infos>
         <CardFooter>
           <div>
-            <h3>{title}</h3>
-            <span>{avaliacao} <img src={estrela} alt="estrela.svg" /></span>
+            <h3>{item.titulo}</h3>
+            <span>{item.avaliacao} <img src={estrela} alt="estrela.svg" /></span>
           </div>
-          <p>{getDescricao(description)}</p>
-          <Link to={`/profile/${id}`}>Saiba Mais</Link>
+          <p>{getDescricao(item.descricao)}</p>
+          <Link to={`/profile/${item.id}`}>Saiba Mais</Link>
         </CardFooter>
       </Card>
   )
