5b2bdd604e19e14393f61b9ad2b64ce64305b258
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-08-14 18:00:21 -0300

    Upload

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,40 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Products } from '../pages/Home'
 
+type itemProduct = {
+    id: number
+    price: number
+}
+
+type Purchase = {
+    products: itemProduct[]
+    delivery: {
+        receiver: string
+        address: {
+            description: string
+            city: string
+            zipCode: string
+            deliveryNumber: string
+            complement: string
+        }
+    }
+    payment: {
+        card: {
+            name: string
+            number: string
+            code: string
+            expires: {
+                month: string
+                year: string
+            }
+        }
+    }
+}
+
+type Response = {
+    orderId: string
+}
+
 const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://fake-api-tau.vercel.app/api/efood/'
@@ -11,12 +45,20 @@ const api = createApi({
         }),
         getMenu: builder.query<Products, string>({
             query: (id) => `restaurantes/${id}`
+        }),
+        purchase: builder.mutation<Response, Purchase>({
+            query: (body) => ({
+                url: 'checkout',
+                method: 'POST',
+                body
+            })
         })
     })
 })
 
 export const {
     useGetRestaurantsQuery,
-    useGetMenuQuery
+    useGetMenuQuery,
+    usePurchaseMutation
 } = api
 export default api
